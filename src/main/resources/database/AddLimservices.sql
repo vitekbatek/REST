ad55df29d861d1b371902df306731fe066d0f6d5
vitekbatek <vitekbatek@gmail.com>
2020-05-28 18:20:35 +0700

    Add records to table limservices

---
+++ src/main/resources/database/AddLimservices.sql
@@ -0,0 +1,3 @@
+insert into limservices (description, comment, lim) values ('hunting', null, 822);
+insert into limservices (description, comment, lim) values ('fishing', null, 752);
+insert into limservices (description, comment, lim) values ('teaching', null, 641);
